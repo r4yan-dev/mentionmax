4254e62ee4593b5bc88cc5350e2363187ab70842
r4yan-dev <aboaaloch@gmail.com>
2026-09-02 16:35:38 +0100

    Add curriculum service abstraction

---
+++ src/services/curriculum/curriculumService.ts
@@ -0,0 +1,27 @@
+import { mockCurriculum } from "../../data/curriculum/mockCurriculum";
+import { getTrackSubjects, subjects, tracks } from "../../data/curriculum/tracks";
+import type { CurriculumNode, Subject, SubjectId, Track, TrackId } from "../../types/academic";
+
+export const curriculumService = {
+  getTracks(): Track[] {
+    return Object.values(tracks);
+  },
+
+  getTrack(trackId: TrackId): Track {
+    return tracks[trackId];
+  },
+
+  getSubjects(trackId: TrackId): Subject[] {
+    return getTrackSubjects(trackId);
+  },
+
+  getSubject(subjectId: SubjectId): Subject {
+    return subjects[subjectId];
+  },
+
+  getNodes(trackId: TrackId, subjectId?: SubjectId): CurriculumNode[] {
+    return mockCurriculum.filter(
+      (node) => node.trackIds.includes(trackId) && (!subjectId || node.subjectId === subjectId),
+    );
+  },
+};
