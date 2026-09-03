6a7eb497a490e2cfb3a426b7a076d2b0b2fc2a7e
r4yan-dev <aboaaloch@gmail.com>
2026-09-04 00:52:50 +0100

    fix: restore router for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
 import Landing from "./pages/Landing";
-export default function App() { return <Landing />; }
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Landing />
+    </BrowserRouter>
+  );
+}
