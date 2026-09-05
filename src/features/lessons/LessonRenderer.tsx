aa3877afe8cdc6f306d27bbc1314c370191f18e3
r4yan-dev <aboaaloch@gmail.com>
2026-09-05 01:13:18 +0100

    style(lessons): use cleaner typography with limited handwriting

--- src/features/lessons/LessonRenderer.tsx
+++ src/features/lessons/LessonRenderer.tsx
@@ -1,6 +1,7 @@
 import { LatexText } from "../../components/ui/LatexText";
 import type { LessonBlock, LessonDocument } from "../../types/academic";
 import "./lesson-renderer.css";
+import "./lesson-renderer-fonts.css";
 
 function VisualBlock({ type, title }: { type: "graph" | "diagram"; title?: string }) {
   if (type === "graph") {
