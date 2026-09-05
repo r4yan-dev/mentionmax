dc145c633c47f84b2cd7db4dffb2033e934db8dc
r4yan-dev <aboaaloch@gmail.com>
2026-09-05 14:03:50 +0100

    Wire detailed primitives and derivation lessons

--- src/services/lesson/lessonService.ts
+++ src/services/lesson/lessonService.ts
@@ -2,12 +2,24 @@ import type { LessonDocument, SubjectId, TrackId } from "../../types/academic";
 import { secondBacLessons } from "../../data/curriculum/secondBac";
 import { secondBacMathLesson2 } from "../../data/curriculum/secondBacMathLesson2";
 import { secondBacMathSequencesLesson } from "../../data/curriculum/secondBacMathSequencesLesson";
+import { secondBacMathPrimitivesLesson } from "../../data/curriculum/secondBacMathPrimitivesLesson";
+import { secondBacMathDerivationLesson } from "../../data/curriculum/secondBacMathDerivationLesson";
 import { secondBacPcLessonsLong } from "./pcLessonsLong";
 import { secondBacSvtLessons, secondBacEnglishLessons, secondBacPhilosophyLessons } from "../../data/curriculum/secondBacHumanLessons";
 
+const dedicatedMathLessonIds = new Set([
+  "2bac-maths-sequences",
+  "2bac-maths-derivation",
+  "2bac-maths-primitives",
+]);
+
 const lessons: LessonDocument[] = [
-  ...secondBacLessons.filter((lesson) => lesson.subjectId !== "physique-chimie" && lesson.id !== "2bac-maths-sequences"),
+  ...secondBacLessons.filter(
+    (lesson) => lesson.subjectId !== "physique-chimie" && !dedicatedMathLessonIds.has(lesson.id),
+  ),
   secondBacMathSequencesLesson,
+  secondBacMathDerivationLesson,
+  secondBacMathPrimitivesLesson,
   secondBacMathLesson2,
   ...secondBacPcLessonsLong.slice(0, 14),
   ...secondBacSvtLessons,
